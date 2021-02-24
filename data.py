bedb2f4cc988bcd07aed9befa72a8097beaed300
Bracha-Eisenstein <57355251+Bracha-Eisenstein@users.noreply.github.com>
2021-02-25 01:10:58 +0200

    Delete data.py

--- data.py
+++
@@ -1,9 +0,0 @@
-class Data:
-    def __init__(self, file):
-        with open(file) as the_file:
-            paths = the_file.read().split("\n")
-        
-    def parse(file):
-        with open(file) as the_file:
-            paths = the_file.read().split("\n")
-        return paths[0], paths[1:]
